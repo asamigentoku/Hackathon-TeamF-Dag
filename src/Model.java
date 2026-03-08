14bca1d11e113d65ff3275e79c9e341641739b2f
asamigentoku <gentoku.asami.9v@stu.hosei.ac.jp>
2026-03-09 00:42:05 +0900

    モデル定義ずみ

--- src/Model.java
+++ src/Model.java
@@ -4,11 +4,13 @@ public class Model {
     String day;
     int period;
     int credit;
-    Model(int id,String name,String day,int period,int credit){
+    boolean required;
+    Model(int id,String name,String day,int period,int credit,boolean required){
         this.id=id;
         this.name=name;
         this.day=day;
         this.period=period;
         this.credit=credit;
+        this.required=required;
     }
 }
