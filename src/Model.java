cff68cc5b726bab7a8ab614f40c93082e34f8b11
asamigentoku <gentoku.asami.9v@stu.hosei.ac.jp>
2026-03-08 15:21:58 +0900

    javaコードまで作成

---
+++ src/Model.java
@@ -0,0 +1,14 @@
+public class Model {
+    int id;
+    String name;
+    String day;
+    int period;
+    int credit;
+    Model(int id,String name,String day,int period,int credit){
+        this.id=id;
+        this.name=name;
+        this.day=day;
+        this.period=period;
+        this.credit=credit;
+    }
+}
